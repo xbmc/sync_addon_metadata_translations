f5f1ae7c086b50535a7f11264598b84b54b7d960
anxdpanic <anxdpanic@users.noreply.github.com>
2020-11-27 14:37:54 -0500

    sync en_GB po lines to untranslated po files

--- sync_addon_metadata_translations/__main__.py
+++ sync_addon_metadata_translations/__main__.py
@@ -100,7 +100,8 @@ def get_po_metadata(po_index, ctxt):
             string = string.replace('"', '')
             string = string.replace('%repdq%', '\\"')
 
-            payload.append((po_file['language_code'], string))
+            if string:
+                payload.append((po_file['language_code'], string))
 
     print('{ctxt} from po files...'.format(ctxt=ctxt))
     print(payload)
@@ -415,13 +416,18 @@ def insert_po_lines(po_index, po_lines):
     languages = list(po_lines.keys())
 
     for index, po_item in enumerate(po_index):
-        if po_item.get('language_code') in languages:
-            insert_index = get_po_insert_index(po_item['content_lines'])
-            if insert_index <= 0:
-                print('Skipped inserting lines into {language_code} po file...'
-                      .format(language_code=po_item.get('language_code')))
+        insert_index = get_po_insert_index(po_item['content_lines'])
+        if insert_index <= 0:
+            print('Skipped inserting lines into {language_code} po file...'
+                  .format(language_code=po_item.get('language_code')))
 
+        insert_lines = []
+        if po_item.get('language_code') in languages:
             insert_lines = po_lines.get(po_item.get('language_code'))
+        elif 'en_GB' in languages:
+            insert_lines = po_lines.get('en_GB')
+
+        if insert_lines:
             insert_lines = format_po_lines(insert_lines)
 
             payload[index]['content_lines'] = po_item['content_lines'][:insert_index] + \
