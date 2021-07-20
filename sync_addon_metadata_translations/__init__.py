63ae7d944ccea7841b07b95fc23e77d02e8066c6
anxdpanic <anxdpanic@users.noreply.github.com>
2021-07-20 16:02:52 -0400

    Add simple docstrings

--- sync_addon_metadata_translations/__init__.py
+++ sync_addon_metadata_translations/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-    Copyright (C) 2020 anxdpanic
+    Copyright (C) 2021 TeamKodi
 
     This file is part of sync_addon_metadata_translations
 
     SPDX-License-Identifier: GPL-3.0-only
-    See LICENSES/GPL-3.0-only.txt for more information.
+    See LICENSES/GPL-3.0-only for more information.
 """
 
 __version__ = '0.0.5'
