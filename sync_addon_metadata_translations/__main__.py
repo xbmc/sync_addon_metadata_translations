9ffda0c1d398a330368343b477bf8950b79e18b5
anxdpanic <anxdpanic@users.noreply.github.com>
2021-09-14 14:25:20 -0400

    fixup indentation for whitespace eof check

--- sync_addon_metadata_translations/__main__.py
+++ sync_addon_metadata_translations/__main__.py
@@ -726,15 +726,15 @@ def insert_po_lines(po_index, po_lines):
                                               insert_lines + \
                                               po_item['content_lines'][insert_index:]
 
-            # remove whitespace from eof
-            for idx, line in reversed(list(enumerate(payload[index]['content_lines']))):
-                if line.strip() == '':
-                    del payload[index]['content_lines'][idx]
-                else:
-                    break
+        # remove whitespace from eof
+        for idx, line in reversed(list(enumerate(payload[index]['content_lines']))):
+            if line.strip() == '':
+                del payload[index]['content_lines'][idx]
+            else:
+                break
 
-            # ensure one new line at end of file
-            po_item['content_lines'] += ['\n']
+        # ensure one new line at end of file
+        po_item['content_lines'] += ['\n']
 
     return payload
 
